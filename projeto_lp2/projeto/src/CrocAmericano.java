5e2b66bf167bc966deb6fe3765ef91847df50eff
Marina Ester <marinalopes1109@gmail.com>
2025-11-05 16:54:59 -0300

    Delete projeto_lp2 directory

--- projeto_lp2/projeto/src/CrocAmericano.java
+++
@@ -1,2 +0,0 @@
-public class CrocAmericano extends CrocSimples implements Produtos{
-}
--- projeto_lp2/projeto/src/HamburguerTriplo.java
+++
@@ -1,6 +0,0 @@
-public class HamburguerTriplo extends Hamburguer implements Produtos{
-    HamburguerTriplo(){
-        super();
-        novoPreco(3);
-    }
-}
